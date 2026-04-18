296d8d59a1fe24c7dc32fd4e44e5cc11d3d5838f
Leafy <100572003+FLeafy@users.noreply.github.com>
2026-04-18 15:37:45 +0700

    feat: create db.js and new pool

---
+++ src/config/db.js
@@ -0,0 +1,17 @@
+const { pool } = require('pg');
+require('dotenv').config();
+
+const pool = new pool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+});
+
+pool.on('error', (err) => {
+  console.error('Unexpected database error', err);
+  process.exit(-1);
+});
+
+modile.exports = pool;
