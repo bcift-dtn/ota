3436f58166f709cf0b7258dd5f9addc69786c74c
Leafy <100572003+FLeafy@users.noreply.github.com>
2026-06-27 04:17:46 +0700

    feat: add mobile design for the navbar, using hamburger

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,6 +1,33 @@
 const profileBtn = document.querySelector('#profileBtn');
 const profileDropdownContainer = document.querySelector('#profileDropdownContainer')
 
+const hamburgerBtn = document.querySelector('#navHamburgerBtn');
+const drawerCloseBtn = document.querySelector('#drawerCloseBtn');
+const mobileDrawer = document.querySelector('#mobileNavDrawer');
+const mobileOverlay = document.querySelector('#mobileNavOverlay');
+const drawerLoginBtn = document.querySelector('#drawerLoginBtn');
+
+function openDrawer() {
+  mobileDrawer.classList.add('is-open');
+  mobileOverlay.classList.add('is-open');
+  document.body.style.overflow = 'hidden';
+}
+
+function closeDrawer() {
+  mobileDrawer.classList.remove('is-open');
+  mobileOverlay.classList.remove('is-open');
+  document.body.style.overflow =  '';
+}
+
+hamburgerBtn?.addEventListener('click', openDrawer);
+mobileOverlay?.addEventListener('click', closeDrawer);
+drawerCloseBtn?.addEventListener('click', closeDrawer);
+
+drawerLoginBtn?.addEventListener('click', () => {
+    closeDrawer();
+    document.querySelector('#navLoginBtn')?.click();
+});
+
 profileBtn?.addEventListener('click', e => {
   e.preventDefault();
 
